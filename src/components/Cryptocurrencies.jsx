118d634ff9224be7a69951b5fb332597c3ce69bb
lobsang norbu <50455820+lobnor2@users.noreply.github.com>
2022-12-30 23:06:27 +0530

    removed input only in home page

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -7,13 +7,11 @@ import { useGetCryptosQuery } from "../services/cryptoApi";
 const Cryptocurrencies = ({ simplified }) => {
   const count = simplified ? 10 : 100;
   const { data: cryptoList, isFetching } = useGetCryptosQuery(count);
-  const [cryptos, setCryptos] = useState(cryptoList?.data?.coins);
+  const [cryptos, setCryptos] = useState([]);
   console.log(cryptos);
   const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
-    setCryptos(cryptoList?.data?.coins);
-
     const filteredData = cryptoList?.data?.coins.filter((coin) =>
       coin.name.toLowerCase().includes(searchTerm.toLowerCase())
     );
@@ -25,12 +23,15 @@ const Cryptocurrencies = ({ simplified }) => {
 
   return (
     <>
-      <div className="search-crypto">
-        <Input
-          placeholder="Search Crypto"
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
-      </div>
+      {!simplified && (
+        <div className="search-crypto">
+          <Input
+            placeholder="Search Crypto"
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </div>
+      )}
+
       <Row gutter={[32, 32]} className="crypto-card-container">
         {cryptos?.map((currency) => (
           <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
