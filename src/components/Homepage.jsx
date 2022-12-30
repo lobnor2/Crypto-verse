ea00a2962a54b2a2044414227b5398dd56ed0af3
lobsang norbu <50455820+lobnor2@users.noreply.github.com>
2022-12-30 13:33:00 +0530

    added coinraking api using rapid api
    got few error cause my free limit is over and i tried coin gecko but lack of data, hence made new account in rapidapi and fetched the data using redux toolkit query

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -9,6 +9,9 @@ const { Title } = Typography;
 const Homepage = () => {
   const { data, isFetching } = useGetCryptosQuery();
   console.log(data);
+  const globalStats = data?.data?.stats;
+
+  if (isFetching) return "Loading...";
   return (
     <>
       <Title level={2} className="heading">
@@ -16,27 +19,42 @@ const Homepage = () => {
       </Title>
       <Row>
         <Col span={12}>
-          <Statistic title="Total Cryptocurrencies" value="5" />{" "}
+          <Statistic
+            title="Total Cryptocurrencies in the world"
+            value={globalStats.total}
+          />{" "}
         </Col>
         <Col span={12}>
-          <Statistic title="Total Exchanges" value="5" />{" "}
+          <Statistic
+            title="Total Exchanges"
+            value={globalStats.totalExchanges}
+          />{" "}
         </Col>
         <Col span={12}>
-          <Statistic title="Total Market Cap" value="5" />{" "}
+          <Statistic
+            title="Total Market Cap"
+            value={millify(globalStats.totalMarketCap)}
+          />{" "}
         </Col>
         <Col span={12}>
-          <Statistic title="Total 24h Volume" value="5" />{" "}
+          <Statistic
+            title="Total 24h Volume"
+            value={millify(globalStats.total24hVolume)}
+          />{" "}
         </Col>
         <Col span={12}>
-          <Statistic title="Total Markets" value="5" />{" "}
+          <Statistic
+            title="Total Markets"
+            value={millify(globalStats.totalMarkets)}
+          />{" "}
         </Col>
       </Row>
-      {data?.map((item) => (
+      {/* {data?.map((item) => (
         <>
           <Title>{item.name}</Title>
           <Image src={item.image.large} width={200} />
         </>
-      ))}
+      ))} */}
     </>
   );
 };
--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,17 +1,19 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const cryptoApiHeaders = {};
-const baseUrl = "https://api.coingecko.com/api/v3";
+const cryptoApiHeaders = {
+  "X-RapidAPI-Key": "f12e68dde8msh7f65513efc51dc6p1ffff9jsn2464e507fe2e",
+  "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
+};
+const baseUrl = "https://coinranking1.p.rapidapi.com";
 
-// const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
 
 export const cryptoApi = createApi({
   reducerPath: "cryptoApi",
   baseQuery: fetchBaseQuery({ baseUrl }),
   endpoints: (builder) => ({
     getCryptos: builder.query({
-      query: () => "/coins",
-      //   createRequest("/exchanges"),
+      query: () => createRequest("/coins"),
     }),
   }),
 });
